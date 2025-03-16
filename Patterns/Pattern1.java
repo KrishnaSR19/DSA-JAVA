b0fb27b7759f04786de794200fef8fc1025eb21f
Krishna <coderks36@gmail.com>
2025-03-16 11:57:11 +0530

    Patterns

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Solution {
+    public void pattern1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        Solution obj = new Solution();
+        obj.pattern1(n);
+    }
+}
