133c145e89af4df01a67be6d361e5428448ed287
Krishna <coderks36@gmail.com>
2025-03-26 21:52:12 +0530

    Rotate String

---
+++ Concept_Basics/Basic_Strings/rotateString.java
@@ -0,0 +1,17 @@
+public class rotateString {
+    public boolean rotateString(String s, String goal) {
+        // Strings must be same length to be rotations of each other
+            if (s.length() != goal.length()) {
+                return false; 
+            }
+            // Try all possible rotations of s
+            for (int i = 0; i < s.length(); i++) {
+                String rotated = s.substring(i) + s.substring(0, i); 
+                if (rotated.equals(goal)) {
+                    return true;  // Return true if a match is found
+                }
+            }
+            return false;  
+        }
+    
+}
