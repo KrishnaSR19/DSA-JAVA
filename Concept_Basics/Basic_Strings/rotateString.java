b708a1360b232a12ad6b5006beed181813b3733f
Krishna <coderks36@gmail.com>
2025-03-26 23:50:19 +0530

    Rotate String

--- Concept_Basics/Basic_Strings/rotateString.java
+++ Concept_Basics/Basic_Strings/rotateString.java
@@ -1,3 +1,8 @@
+// Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+// A shift on s consists of moving the leftmost character of s to the rightmost position.
+// For example, if s = "abcde", then it will be "bcdea" after one shift
+
+
 public class rotateString {
     public boolean rotateString(String s, String goal) {
         // Strings must be same length to be rotations of each other
