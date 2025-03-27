0592860d42ca8f4478b41fec962feddc0a50ad28
Krishna <coderks36@gmail.com>
2025-03-27 23:04:19 +0530

    anagram optimal

--- Concept_Basics/Basic_Strings/anagram.java
+++ Concept_Basics/Basic_Strings/anagram.java
@@ -23,6 +23,9 @@ public class anagram {
 }
 
 //Brute Force
+// Time Complexity: O(N log N) due to sorting each string.
+
+// Space Complexity: O(1) as no additional data structures are used. Note that for Java, the space complexity will be O(N) due to the creation of additional character arrays.
 import java.util.Arrays;
 
 class Solution {
@@ -48,5 +51,38 @@ class Solution {
         System.out.println(result ? "True" : "False");
     }
 }
+
+
+// Optimal
+// Time Complexity: O(N), where N is the length of the string
+
+// Space Complexity: O(1) as no additional data structures are used
+
+class Solution {
+    public boolean anagramStrings(String s, String t) {
+        // Edge Cases
+        if (s.length() != t.length()) return false;
+
+        // To store the count of each character
+        int[] count = new int[26];
+
+        // Count occurrence of each character in first string 
+        for (char c : s.toCharArray()) count[c - 'a']++;
+
+        // Decrement the count for each character in the second string
+        for (char c : t.toCharArray()) count[c - 'a']--;
+
+        // Check for count of every character
+        for (int i : count) {
+            // If the count is not zero
+            if (i != 0) return false; // Return false
+        }
+
+        // Otherwise strings are anagram
+        return true;
+    }
+}
+
+
 
 
