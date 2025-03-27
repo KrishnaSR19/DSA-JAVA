0ddc8ecfbce0bd302617600d0351dbe6dbab1b7b
Krishna <coderks36@gmail.com>
2025-03-27 22:59:11 +0530

    anagram brute

--- Concept_Basics/Basic_Strings/anagram.java
+++ Concept_Basics/Basic_Strings/anagram.java
@@ -23,4 +23,30 @@ public class anagram {
 }
 
 //Brute Force
+import java.util.Arrays;
+
+class Solution {
+    public boolean anagramStrings(String s, String t) {
+        // If lengths are not equal, they cannot be anagrams
+        if (s.length() != t.length()) return false;
+
+        // Convert strings to char arrays and sort them
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        // Compare sorted arrays
+        return Arrays.equals(sArray, tArray);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String str1 = "INTEGER";
+        String str2 = "TEGERNI";
+        boolean result = solution.anagramStrings(str1, str2);
+        System.out.println(result ? "True" : "False");
+    }
+}
+
 
