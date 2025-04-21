a200d5ac3cf041d7262ce680f00309f4df1e6486
Krishna <coderks36@gmail.com>
2025-04-21 23:00:04 +0530

    Optimal

--- Arrays/Fundamentals/RotatebyK.java
+++ Arrays/Fundamentals/RotatebyK.java
@@ -45,3 +45,37 @@ public class RotatebyK {
         }
     }
 }
+
+
+/*
+  Optimal
+ * Time Complexity: O(N), where N is the size of the array
+As three reversals are performed taking O(k), O(N-k) and O(N) time respectively.
+Space Complexity: O(1), as no extra space is used .
+ */
+
+ // Function to reverse the array between start and end
+ private void reverseArray(int[] nums, int start, int end) {
+    while (start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+// Function to rotate the array to the left by k positions
+public void rotateArray(int[] nums, int k) {
+    int n = nums.length; // Size of array
+    k = k % n; // To avoid unnecessary rotations
+    
+    // Reverse the first k elements
+    reverseArray(nums, 0, k - 1);
+
+    // Reverse the last n-k elements
+    reverseArray(nums, k, n - 1);
+
+    // Reverse the entire array
+    reverseArray(nums, 0, n - 1);
+}
