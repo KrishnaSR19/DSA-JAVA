91fc17b6d016a15b30da963a3d0e641e3259aa98
Krishna <coderks36@gmail.com>
2025-04-21 22:51:43 +0530

    Brute force

--- Arrays/Fundamentals/RotatebyK.java
+++ Arrays/Fundamentals/RotatebyK.java
@@ -14,7 +14,34 @@ public void rotateArray(int[] nums, int k) {
 }
 
 
+/*
+ * Brute Force
+ * Time Complexity: O(N), where N is the length of the array.
+Three loops are used taking K, N-K, and K iterations respectively contributing to O(N+K). However, K can be N/2 in the worst case boiling down the time complexity as O(N).
+Space Complexity: O(K), due to the temporary list created to copy the K elements.
+ */
 
 public class RotatebyK {
-    
+    // Function to rotate the array to the left by k positions
+    public void rotateArray(int[] nums, int k) {
+        int n = nums.length; // Size of array
+        k = k % n; // To avoid unnecessary rotations
+
+        int[] temp = new int[k];
+
+        // Store first k elements in a temporary array
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[i];
+        }
+
+        // Shift n-k elements of given array to the front
+        for (int i = k; i < n; i++) {
+            nums[i - k] = nums[i];
+        }
+
+        // Copy back the k elements at the end
+        for (int i = 0; i < k; i++) {
+            nums[n - k + i] = temp[i];
+        }
+    }
 }
