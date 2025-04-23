9ad91dae478b78302d54b1763e3d67774bf2dca7
Krishna <coderks36@gmail.com>
2025-04-23 22:31:52 +0530

    optimal

--- Arrays/LogicBuilding/RemoveDulpli.java
+++ Arrays/LogicBuilding/RemoveDulpli.java
@@ -1,5 +1,9 @@
+/*
+ * Brute Force
+ */
+
+import java.util.*;
 public class RemoveDulpli {
-    import java.util.*;
         // Function to remove duplicates from the array
         public int removeDuplicates(int[] nums) {
             
@@ -24,3 +28,43 @@ public class RemoveDulpli {
             return k;
         }
 }
+
+/*
+ * Optimal
+ */
+
+ import java.util.*;
+
+     // Function to remove duplicates from the array
+     public int removeDuplicates(int[] nums) {
+         // Edge case: if array is empty
+         if (nums.length == 0) {
+             return 0;
+         }
+         
+         // Initialize pointer for unique elements
+         int i = 0;
+         
+         // Iterate through the array
+         for (int j = 1; j < nums.length; j++) {
+             /*If current element is different 
+             from the previous unique element*/
+             if (nums[i] != nums[j]) {
+                 /* Move to the next position in 
+                 the array for the unique element*/
+                 i++;
+                 /* Update the current position 
+                    with the unique element*/
+                 nums[i] = nums[j];
+             }
+         }
+         
+         // Return the number of unique elements
+         return i + 1;
+     }
+ 
+ 
+
+
+ 
+ 
