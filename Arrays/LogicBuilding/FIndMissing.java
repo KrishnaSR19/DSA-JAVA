97e8900dbfabf5beb48d3363837cc11067e01f1e
Krishna <coderks36@gmail.com>
2025-04-25 21:24:38 +0530

    optimal - 1

--- Arrays/LogicBuilding/FIndMissing.java
+++ Arrays/LogicBuilding/FIndMissing.java
@@ -74,3 +74,29 @@ class Solution {
 
 
 
+/*
+ * Optimal -1
+ */
+
+ public int missingNumber(int[] nums) {
+    // Calculate N from the length of nums
+    int N = nums.length;
+    
+    // Summation of first N natural numbers
+    int sum1 = (N * (N + 1)) / 2;
+    
+    // Summation of all elements in nums
+    int sum2 = 0;
+    for (int num : nums) {
+        sum2 += num;
+    }
+    
+    // Calculate the missing number
+    int missingNum = sum1 - sum2;
+    
+    // Return the missing number
+    return missingNum;
+}
+
+
+
