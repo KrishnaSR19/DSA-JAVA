5c63510cc1f81c08b36a45613fdbfef7471c05d5
Krishna <coderks36@gmail.com>
2025-04-26 21:20:44 +0530

    minor change

--- Arrays/LogicBuilding/UnionOfArrays.java
+++ Arrays/LogicBuilding/UnionOfArrays.java
@@ -1,3 +1,9 @@
+
+/*
+ * Q.Given two sorted arrays nums1 and nums2, return an array that contains the union of these two arrays. The elements in the union must be in ascending order.
+The union of two arrays is an array where all values are distinct and are present in either the first array, the second array, or both.
+ */
+
 /*
  * Brute Force
  * Time Complexity: O( (M+N)log(M+N) ), at max set can store M+N elements {when there are no common elements and elements in nums1 , nums2 are distntict}. So Inserting M+N th element takes log(M+N) time. Upon approximation across inserting all elements in worst, it would take O((M+N)log(M+N) time.
