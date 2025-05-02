70b7b16981529a324e50c86417fb19d2a4099154
Krishna <coderks36@gmail.com>
2025-05-02 09:32:41 +0530

    Rearrange Elements  in Array by sign (Optimal)

--- Arrays/FAQs/RearrangeBySign.java
+++ Arrays/FAQs/RearrangeBySign.java
@@ -94,3 +94,61 @@ class Solution {
     }
 }
 
+/*
+ * Optimal
+ * Time Complexity: O(N), for traversing the array only once where N is the length of the array.
+Space Complexity: O(N) to store the resultant array.
+ */
+
+ import java.util.*;
+
+class Solution {
+    //Function to rearrange elements by their sign
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        
+        // Initialize a result vector of size n
+        int[] ans = new int[n]; 
+        
+         /* Initialize indices for positive
+         and negative elements*/
+        int posIndex = 0, negIndex = 1;
+        
+        // Traverse through each element in nums
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                
+                /* If current element is negative, place
+                it at the next odd index in ans*/
+                ans[negIndex] = nums[i];
+                // Move to the next odd index
+                negIndex += 2;
+                
+            } else {
+                ans[posIndex] = nums[i];
+
+                // Move to the next even index
+                posIndex += 2;
+                
+            }
+        }
+        
+        // Return the rearranged array
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, 2, -4, -5}; 
+        
+        // Create an instance of the Solution class
+        Solution sol = new Solution();
+        
+        int[] ans = sol.rearrangeArray(A);  
+        
+        // Print the rearranged array
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+    }
+}
+
