c3bf31cc809214e4e5f669e526a36c67b9b39d60
Krishna <coderks36@gmail.com>
2025-05-19 22:34:14 +0530

    Sum of 4  number in array should be equal to target(Better)

--- Arrays/LogicBuilding/FourSum.java
+++ Arrays/LogicBuilding/FourSum.java
@@ -56,3 +56,69 @@ class Solution {
     }
 }
 }
+
+
+
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        
+        // Set to store unique quadruplets
+        Set<List<Integer>> set = new HashSet<>();
+        
+        // Checking all possible quadruplets
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                // Set to store elements seen so far in the loop
+                Set<Long> hashset = new HashSet<>();
+                
+                for (int k = j + 1; k < n; k++) {
+                    /* Calculate the fourth element
+                    needed to reach target*/
+                    long sum = (long) nums[i] + nums[j] + nums[k];
+                    long fourth = target - sum;
+                    
+                    /* Find if fourth element exists in 
+                    hashset (complements seen so far)*/
+                    if (hashset.contains(fourth)) {
+                        // Found a quadruplet that sums up to target
+                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], (int) fourth);
+                        Collections.sort(temp);
+                        set.add(temp);
+                    }
+                    
+                    // Insert the kth element into hashset for future checks
+                    hashset.add((long) nums[k]);
+                }
+            }
+        }
+        
+        // Convert set to list (unique quadruplets)
+        ans.addAll(set);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+        int target = 9;
+        
+        // Create an instance of Solution class
+        Solution sol = new Solution();
+        
+        List<List<Integer>> ans = sol.fourSum(nums, target);
+        
+        // Print the result
+        System.out.println("The quadruplets are:");
+        for (List<Integer> quad : ans) {
+            System.out.print("[");
+            for (int num : quad) {
+                System.out.print(num + " ");
+            }
+            System.out.println("]");
+        }
+    }
+}
+
