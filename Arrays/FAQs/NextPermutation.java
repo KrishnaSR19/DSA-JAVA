e1790955b08d0b084d11d20984c57cd702210956
Krishna <coderks36@gmail.com>
2025-06-05 09:59:46 +0530

    NextPermuatation(Optimal Force)

--- Arrays/FAQs/NextPermutation.java
+++ Arrays/FAQs/NextPermutation.java
@@ -111,5 +111,83 @@ The function next_permutation() from the <algorithm> header modifies the array i
 
   /*
    * Optimal
+   * Time Complexity: O(N), where N is the size of the input array.
+Finding the pivot takes O(N) time. Finding the next greater element also takes O(N) in the worst case. And, reversing the subarray takes O(N). All this adds up to a total of O(N) time complexity.
+Space Complexity: O(1), as the modification is done in-place and no extra data structure was used apart from a few variables.
    * 
    */
+
+  import java.util.*;
+
+class Solution {
+    // Function to get the next permutation of given array
+    public void nextPermutation(int[] nums) {
+        int n = nums.length; // Size of the given array
+        
+        // To store the index of the first smaller element from right
+        int ind = -1; 
+        
+        // Find the first index from the end where nums[i] < nums[i+1]
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+        
+        /* If no such index exists, array is in descending order
+           So, reverse it to get the smallest permutation */
+        if(ind == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+        
+        // Find the element just greater than nums[ind] from the end
+        for(int i = n - 1; i > ind; i--) {
+            if(nums[i] > nums[ind]) {
+                swap(nums, i, ind); // Swap with nums[ind]
+                break;
+            }
+        }
+        
+        // Reverse the right half to get the next smallest permutation
+        reverse(nums, ind + 1, n - 1);
+        return;
+    }
+    
+    // Helper Function to swap two numbers in an array
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    // Helper function to reverse the array
+    private void reverse(int[] nums, int start, int end) {
+        while(start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+
+        /* Creating an instance of 
+           Solution class */
+        Solution sol = new Solution();
+
+        // Output the original array
+        System.out.print("Given array: ");
+        for(int x : nums) System.out.print(x + " ");
+
+        // Function call to get the next permutation of given array
+        sol.nextPermutation(nums);
+
+        // Output the next permutation
+        System.out.print("\nNext Permutation: ");
+        for(int x : nums) System.out.print(x + " ");
+    }
+}
