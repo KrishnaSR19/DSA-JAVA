1684be5c9fe8edcd4b88a6bc4f4a8a76cda5b63e
Krishna <coderks36@gmail.com>
2025-06-05 07:16:14 +0530

    Kadane's Algo(Follow to print array with max sum )

--- Arrays/FAQs/KadanesAlgorithm.java
+++ Arrays/FAQs/KadanesAlgorithm.java
@@ -157,3 +157,63 @@ class Solution {
         System.out.println("The maximum subarray sum is: " + maxSum);
     }
 }
+
+
+/*
+ * Follow Up(to print the subarray with max sum)
+ * Time Complexity: O(N), for using a single loop running N times, where N is the size of the array.
+Space Complexity: O(1), for not using any extra space.
+ */
+
+ class Solution {
+   // Function to find maximum sum of subarrays and print the subarray having maximum sum
+    public int maxSubArray(int[] nums) {
+        
+        // maximum sum
+        long maxi = Long.MIN_VALUE; 
+        
+        // current sum of subarray
+        long sum = 0; 
+        
+        // starting index of current subarray
+        int start = 0; 
+        
+        // indices of the maximum sum subarray
+        int ansStart = -1, ansEnd = -1; 
+        
+        // Iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+            
+            // update starting index if sum is reset
+            if (sum == 0) {
+                start = i;
+            }
+            
+            // add current element to the sum
+            sum += nums[i]; 
+            
+            /* Update maxi and subarray indices
+            if current sum is greater */
+            if (sum > maxi) {
+                maxi = sum;
+                ansStart = start;
+                ansEnd = i;
+            }
+            
+            // Reset sum to 0 if it becomes negative
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        
+        // Printing the subarray
+        System.out.print("The subarray is: [");
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println("]");
+
+        // Return the maximum subarray sum found
+        return (int) maxi;
+    }
+}
