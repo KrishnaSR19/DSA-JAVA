7c77926df91a97b003c435cd2b91e65904c1bd09
Krishna <coderks36@gmail.com>
2025-06-05 07:06:36 +0530

    Kadane's Algo(Optimal Approach)

--- Arrays/FAQs/KadanesAlgorithm.java
+++ Arrays/FAQs/KadanesAlgorithm.java
@@ -103,3 +103,57 @@ class Solution {
         System.out.println("The maximum subarray sum is: " + maxSum);
     }
 }
+
+
+
+/*
+ * Optimal
+ * Time Complexity: O(N) for single traversal, here N is the size of the array.
+Space Complexity: O(1), for not using any extra space.
+ */
+
+ import java.util.*;
+
+class Solution {
+    // Function to find maximum sum of subarrays
+    public int maxSubArray(int[] nums) {
+        
+        // maximum sum
+        long maxi = Long.MIN_VALUE; 
+        
+        //current sum of subarray 
+        long sum = 0; 
+        
+        // Iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+            
+            // Add current element to the sum
+            sum += nums[i]; 
+            
+            // Update maxi if current sum is greater
+            if (sum > maxi) {
+                maxi = sum; 
+            }
+            
+            // Reset sum to 0 if it becomes negative
+            if (sum < 0) {
+                sum = 0; 
+            }
+        }
+        
+        // Return the maximum subarray sum found
+        return (int) maxi;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+
+        // Create an instance of Solution class
+        Solution sol = new Solution();
+
+        int maxSum = sol.maxSubArray(arr);
+
+        // Print the max subarray sum
+        System.out.println("The maximum subarray sum is: " + maxSum);
+    }
+}
