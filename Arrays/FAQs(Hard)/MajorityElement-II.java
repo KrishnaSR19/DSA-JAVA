62bef24a1457a518ef0e70148a73f6550484c128
Krishna <coderks36@gmail.com>
2025-06-07 10:27:03 +0530

    Majority element(>n/3) in array(Better Approach)

--- Arrays/FAQs(Hard)/MajorityElement-II.java
+++ Arrays/FAQs(Hard)/MajorityElement-II.java
@@ -67,3 +67,65 @@ class Solution {
         System.out.println();
     }
 }
+
+/*
+ * Better(HashMap)
+ * Time Complexity: O(N * logN), where N is size of the given array. For using a map data structure, where insertion in the map takes logN time. And we are doing it for N elements. So, it results in the first term O(N*logN). On using unordered_map instead, the first term will be O(N) for the best and average case and for the worst case, it will be O(N2).
+
+Space Complexity: O(N) for uing a map data structure. A list that stores a maximum of 2 elements is also used, but that space used is so small that it can be considered constant.
+ */
+
+ import java.util.*;
+
+class Solution {
+    // Function to find majority elements in an array
+    public List<Integer> majorityElementTwo(int[] nums) {
+        
+        // Size of the array
+        int n = nums.length;
+
+        // List of answers
+        List<Integer> result = new ArrayList<>();
+
+        // Declaring a map
+        Map<Integer, Integer> mpp = new HashMap<>();
+
+        // Least occurrence of the majority element
+        int mini = n / 3 + 1;
+
+        // Storing the elements with its occurrence
+        for (int i = 0; i < n; i++) {
+            mpp.put(nums[i], mpp.getOrDefault(nums[i], 0) + 1);
+
+            // Checking if nums[i] is the majority element
+            if (mpp.get(nums[i]) == mini) {
+                result.add(nums[i]);
+            }
+
+            // If result size is equal to 2 break out of loop
+            if (result.size() == 2) {
+                break;
+            }
+        }
+
+        // Return the majority elements
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {11, 33, 33, 11, 33, 11};
+
+        // Create an instance of Solution class
+        Solution sol = new Solution();
+
+        List<Integer> ans = sol.majorityElementTwo(arr);
+
+        // Print the majority elements found
+        System.out.print("The majority elements are: ");
+        for (int it : ans) {
+            System.out.print(it + " ");
+        }
+        System.out.println();
+    }
+}
+
