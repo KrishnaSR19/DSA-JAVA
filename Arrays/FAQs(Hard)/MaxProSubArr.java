2be7536454520a2f0126ea0942c994cbdb36a7e6
Krishna <coderks36@gmail.com>
2025-06-13 18:24:10 +0530

    Gives max product from the subarrays(Better)

--- Arrays/FAQs(Hard)/MaxProSubArr.java
+++ Arrays/FAQs(Hard)/MaxProSubArr.java
@@ -5,7 +5,9 @@ A subarray is a contiguous non-empty sequence of elements within an array.
 
  /*
   * Brute Force
+Time Complexity: O(N3) for using 3 nested loops for finding all possible subarrays and their product. Here N is the size of the array.
 
+Space Complexity: O(1), as no additional space is used apart from the input array.
   */
 
 
@@ -50,3 +52,61 @@ class Main {
         System.out.println("The maximum product subarray: " + maxProd);
     }
 }
+
+
+/*
+ * Better
+ * Time Complexity: O(N2) for using 2 nested loops for finding all possible subarrays and their product. Here N is the size of the array.
+
+Space Complexity: O(1) as no additional space is used apart from the input array.
+ */
+
+
+ import java.util.*;
+
+class Solution {
+    // Function to find maximum product subarray
+    public int maxProduct(int[] nums) {
+        // Initialize result with first element of nums
+        int result = nums[0];
+
+        /* Iterate through each element
+        as a starting point of subarray */
+        for (int i = 0; i < nums.length; i++) {
+            // Initialize p with nums[i]
+            int p = nums[i];
+
+            /* Iterate through subsequent elements
+            to form subarrays starting from nums[i] */
+            for (int j = i + 1; j < nums.length; j++) {
+                
+                /* Update result with the
+                max of current result and p */
+                result = Math.max(result, p);
+
+                // Update p by multiplying with nums[j]
+                p *= nums[j];
+            }
+
+            // Update result for subarray ending at nums[i]
+            result = Math.max(result, p);
+        }
+
+        // Return maximum product subarray found
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 3, 7, 1, 2};
+
+        // Create an instance of the Solution class
+        Solution sol = new Solution();
+
+        int maxProd = sol.maxProduct(nums);
+
+        // Print the result
+        System.out.println("The maximum product subarray: " + maxProd);
+    }
+}
+
+
