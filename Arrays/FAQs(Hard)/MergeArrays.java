6c0e243b701e5400288e03b6c44e3b190bdbd430
Krishna <coderks36@gmail.com>
2025-06-14 12:26:07 +0530

    Merging two arrays(Optimal-I(2 pointer & sort ) )

--- Arrays/FAQs(Hard)/MergeArrays.java
+++ Arrays/FAQs(Hard)/MergeArrays.java
@@ -15,7 +15,10 @@ Space Complexity: O(N+M) for using an extra array of size N+M.
 
 
 // Function to merge two sorted arrays nums1 and nums2
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+import java.util.Arrays;
+
+public void merge(int[] nums1, int m, int[] nums2, int n) {
         
         // Declare a 3rd array and 2 pointers:
         int[] merged = new int[m + n];
@@ -49,3 +52,52 @@ Space Complexity: O(N+M) for using an extra array of size N+M.
             nums1[i] = merged[i];
         }
     }
+
+
+    /*
+     * Optimal-I
+     * Time Complexity: O(min(N, M)) + O(NxlogN) + O(MxlogM), where N and M are the sizes of the given arrays. O(min(N, M)) is for swapping the array elements. And O(NxlogN) and O(MxlogM) are for sorting the two arrays.
+
+Space Complexity: O(1) as no additional space is used apart from the input array.
+
+
+
+
+     */
+
+
+    // Function to merge two sorted arrays nums1 and nums2
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Pointer for nums1 (end of valid elements)
+        int left = m - 1;
+        
+        // Pointer for nums2 (beginning of valid elements)
+        int right = 0;
+        
+        /* Swap the elements until nums1[left]
+        is smaller than nums2[right]*/
+        while (left >= 0 && right < n) {
+            if (nums1[left] > nums2[right]) {
+                int temp = nums1[left];
+                nums1[left] = nums2[right];
+                nums2[right] = temp;
+                left--;
+                right++;
+            } 
+            //break otherwise
+            else break;
+        }
+        
+        // Sort nums1 from index 0 to m-1
+        Arrays.sort(nums1, 0, m);
+        
+        // Sort nums2 from start to end
+        Arrays.sort(nums2);
+        
+        // Put the elements of nums2 in nums1
+        for (int i = m; i < m + n; i++) {
+            nums1[i] = nums2[i - m];
+        }
+    }
+    
+  
