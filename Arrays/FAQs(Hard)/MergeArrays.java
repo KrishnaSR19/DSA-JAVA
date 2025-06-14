e0af90b47e3c4a283821a6d9597d46d3ab67149b
Krishna <coderks36@gmail.com>
2025-06-14 18:17:49 +0530

    Merging two arrays(Optimal-II(gap method ) )

--- Arrays/FAQs(Hard)/MergeArrays.java
+++ Arrays/FAQs(Hard)/MergeArrays.java
@@ -60,9 +60,6 @@ public void merge(int[] nums1, int m, int[] nums2, int n) {
 
 Space Complexity: O(1) as no additional space is used apart from the input array.
 
-
-
-
      */
 
 
@@ -99,5 +96,64 @@ Space Complexity: O(1) as no additional space is used apart from the input array
             nums1[i] = nums2[i - m];
         }
     }
+
+
+    /*
+     * Optimal -II
+     * Time Complexity: O((N+M)xlog(N+M)), where N and M are the sizes of the given arrays. The gap is ranging from N+M to 1 and every time the gap gets divided by 2. So, the time complexity of the outer loop will be O(log(N+M)). Now, for each value of the gap, the inner loop can at most run for (N+M) times. So, the time complexity of the inner loop will be O(N+M). So, the overall time complexity will be O((N+M)xlog(N+M)).
+Space Complexity: O(1) as no additional space is used apart from the input array.
+     */
+
+    // Function to merge two sorted arrays nums1 and nums2
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int len = n + m;
+        int gap = (len / 2) + (len % 2);
+
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+            while (right < len) {
+                
+                // When left in nums1[] and right in nums2[]
+                if (left < m && right >= m) {
+                    swapIfGreater(nums1, nums2, left, right - m);
+                }
+                // When both pointers in nums2[]
+                else if (left >= m) {
+                    swapIfGreater(nums2, nums2, left - m, right - m);
+                }
+                // When both pointers in nums1[]
+                else {
+                    swapIfGreater(nums1, nums1, left, right);
+                }
+                // Increment the pointers by 1 each
+                left++;
+                right++;
+            }
+            // If gap is equal, break out of the loop
+            if (gap == 1)
+                break;
+            gap = (gap / 2) + (gap % 2);
+        }
+
+        // Copy elements of nums2 into nums1
+        for (int i = m; i < m + n; i++) {
+            nums1[i] = nums2[i - m];
+        }
+    }
+
+    // Utility function to swap elements if needed
+    private void swapIfGreater(int[] arr1, int[] arr2, int idx1, int idx2) {
+        if (arr1[idx1] > arr2[idx2]) {
+            
+            int temp = arr1[idx1];
+            arr1[idx1] = arr2[idx2];
+            arr2[idx2] = temp;
+        }
+    }
+
+
+
     
   
