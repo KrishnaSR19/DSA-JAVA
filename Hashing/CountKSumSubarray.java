77495c0954e746f8da765e7346415bfcb813cc58
Krishna <coderks36@gmail.com>
2025-06-18 11:13:11 +0530

    count Subarray with sum K(Optimal)

--- Hashing/CountKSumSubarray.java
+++ Hashing/CountKSumSubarray.java
@@ -33,6 +33,12 @@ public int subarraySum(int[] nums, int k) {
 
 /*
  * Brute Force Optimized to O(N2)
+ * Time Complexity: O(N2), where N is the size of the array. We are using two
+ * nested loops here, each running for approximately N times. Therefore, the
+ * time complexity will be approximately O(N2).
+ * 
+ * Space Complexity: O(1), as we are not using any extra space to solve this
+ * problem.
  */
 class Solution {
     public int subarraySum(int[] nums, int k) {
@@ -51,4 +57,37 @@ class Solution {
 
         return count;
     }
+}
+
+/*
+ * Optimal
+ * Time Complexity: O(N) or O(NxlogN) depending on the map data structure used,
+ * where N is the size of the array. For example, if we use an unordered_map in
+ * C++, the time complexity will be O(N), but if we use a map, the time
+ * complexity will be O(NxlogN). The minimum complexity is O(N) as we are using
+ * a single loop to traverse the array.
+ * 
+ * Space Complexity: O(N) as we are using a map data structure.
+ */
+
+public int subarraySum(int[] nums, int k) {
+    int n = nums.length;
+    // Number of subarrays
+    int count = 0;
+
+    // starting index
+    for (int startIndex = 0; startIndex < n; startIndex++) {
+        int currentSum = 0;
+        // ending index
+        for (int endIndex = startIndex; endIndex < n; endIndex++) {
+            // calculate the sum of subarray [startIndex...endIndex]
+            // sum of [startIndex..endIndex-1] + nums[endIndex]
+            currentSum += nums[endIndex];
+
+            // Increase the count if currentSum == k:
+            if (currentSum == k)
+                count++;
+        }
+    }
+    return count;
 }
