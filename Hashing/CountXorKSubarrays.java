f3167ceedcc957d47e43a64968084a372d63540c
Krishna <coderks36@gmail.com>
2025-06-18 12:14:26 +0530

    Count Subarray with XOR K(Optimal using Hash)

--- Hashing/CountXorKSubarrays.java
+++ Hashing/CountXorKSubarrays.java
@@ -9,6 +9,8 @@
 Space Complexity: O(1) since we are not using any additional space.
  */
 
+import java.util.HashMap;
+import java.util.Map;
 
 public int subarraysWithXorK(int[] nums, int k) {
     int n = nums.length;
@@ -41,7 +43,6 @@ public int subarraysWithXorK(int[] nums, int k) {
  * Space Complexity: O(1) as we are not using any additional space.
  */
 
-
 public int subarraysWithXorK(int[] nums, int k) {
     int n = nums.length;
     int cnt = 0;
@@ -60,6 +61,41 @@ public int subarraysWithXorK(int[] nums, int k) {
             if (xorr == k)
                 cnt++;
         }
+    }
+    return cnt;
+}
+
+/*
+ * Optimal
+ * Time Complexity: O(N) or O(NxlogN), where N is the size of the array. If we
+ * use an unordered_map in C++, the time complexity is O(N). However, with a map
+ * data structure, the time complexity is O(NxlogN). In the worst case for an
+ * unordered_map, the searching time can increase to O(N), making the overall
+ * time complexity O(N2).
+ * 
+ * Space Complexity: O(N), as we are using a map data structure.
+ */
+
+public int subarraysWithXorK(int[] nums, int k) {
+    int n = nums.length;
+    int xr = 0;
+    Map<Integer, Integer> mpp = new HashMap<>();
+    // setting the value of 0.
+    mpp.put(xr, mpp.getOrDefault(xr, 0) + 1);
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        // prefix XOR till index i:
+        xr = xr ^ nums[i];
+
+        // By formula: x = xr ^ k:
+        int x = xr ^ k;
+
+        // add the occurrence of xr ^ k to the count:
+        cnt += mpp.getOrDefault(x, 0);
+
+        // Insert the prefix xor till index i into the map:
+        mpp.put(xr, mpp.getOrDefault(xr, 0) + 1);
     }
     return cnt;
 }
