e60fe35f58fcc0cb77f607f32e71fe6267a08c31
Krishna <coderks36@gmail.com>
2025-06-25 09:39:36 +0530

    FInd square root(Optimal)

--- Binary Search/On answers/FindSqrt.java
+++ Binary Search/On answers/FindSqrt.java
@@ -41,3 +41,37 @@ class Solution {
         return -1;
     }
 }
+
+
+/*
+ * Optimal
+ * Time Complexity:O(logN), where N is the given number. We are basically using the Binary Search algorithm.
+Space Complexity: As no additional space is used, so the Space Complexity is O(1).
+ */
+
+class Solution {
+    public long floorSqrt(long n) {
+        long low = 1, high = n;
+        
+        // Binary search on the answer space
+        while (low <= high) {
+            long mid = (long)(low + high) / 2;
+            long val = mid * mid;
+            
+            // Check if val is less than or equal to n
+            if (val <= (long)(n)) {
+                // Move to the right part
+                low = (int)(mid + 1);
+            } else {
+                // Move to the left part
+                high = (int)(mid - 1);
+            }
+        }
+        
+        // Return the floor of square root
+        return high;
+    }
+}
+
+
+ 
