825591a8dd8423a2aa1641be4de2da31db8ffc19
Krishna <coderks36@gmail.com>
2025-07-28 22:36:50 +0530

    N Queens (more efficient)

--- Recursion/FAQ(Hard)/NQueen.java
+++ Recursion/FAQ(Hard)/NQueen.java
@@ -97,3 +97,81 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+/*
+ * More Efficient 
+ */
+
+
+ class Solution {
+
+    public List<List<String>> answer = new ArrayList<>();
+
+    public boolean isSafe(char[][] board, int row, int col){
+        //vertical
+        for(int i=row; i>=0; i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //diagonal left up
+        for(int i = row-1, j = col-1; i>=0 && j>=0; i-- , j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        //diagonal right up
+        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    //adding each solution in a row
+    public void addInList(char[][] board){
+        List<String> str = new ArrayList<>();
+        
+        //adding each row as a string in the list         
+        for(char[] row:board){
+            str.add(new String(row));
+        }
+
+        answer.add(str); //adding in the final List
+    }
+
+    public void nQueens(char[][] board, int row ){
+
+        //base
+        if(row == board.length){
+            addInList(board);
+            return;
+        }
+
+        //recursion
+        for(int i=0; i < board.length; i++){
+            if(isSafe(board, row, i)){ //checking if no other queen attacks at current position
+                board[row][i] = 'Q'; //adding the queen to the square
+                nQueens(board, row+1); //adding queen to next row
+                board[row][i] = '.'; //Backtrack: Remove queen
+            }
+        }
+
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+       
+        //declaring all the elements as '.'
+        for(char [] row: board){
+            Arrays.fill(row, '.');
+        }
+        nQueens(board, 0);
+        return answer;
+    }
+}
