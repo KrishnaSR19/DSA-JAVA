86beca80b68f5dd7a6528beb6c9a67f52128e113
Krishna <coderks36@gmail.com>
2025-08-10 09:02:21 +0530

    Segregate odd and even indices(Optimal)

--- LinkedList/Logic Building/SegregateOddEven.java
+++ LinkedList/Logic Building/SegregateOddEven.java
@@ -35,11 +35,9 @@ class Solution {
         // Check if list is empty or has only one node
         if (head == null || head.next == null)
             return head;
-
         // To store values
         List<Integer> array = new ArrayList<>();
         ListNode temp = head;
-
         /*
          * Traverse the list, skipping one node,
          * and store values in the vector
@@ -48,7 +46,6 @@ class Solution {
             array.add(temp.val);
             temp = temp.next.next;
         }
-
         /*
          * If there's an even number
          * of nodes, add the value
@@ -59,7 +56,6 @@ class Solution {
 
         // Reset temp
         temp = head.next;
-
         /*
          * Traverse the list again, skipping one node,
          * and store values
@@ -69,7 +65,6 @@ class Solution {
             array.add(temp.val);
             temp = temp.next.next;
         }
-
         /*
          * If there's an odd number
          * of nodes, add the
@@ -81,23 +76,71 @@ class Solution {
         // Reset temp
         temp = head;
         int i = 0;
-
         // Update node values
         while (temp != null) {
             temp.val = array.get(i);
             temp = temp.next;
             i++;
         }
-
         return head;
     }
 }
 
-// Function to print the linked list
-public static void printLL(ListNode head) {
-    while (head != null) {
-        System.out.print(head.val + " ");
-        head = head.next;
+/*
+ * Optimal
+ * Time Complexity: O(N/2)x2 ~ O(N) because we are iterating over the
+ * odd-indexed as well as the even-indexed elements. Here N is the size of the
+ * given linked list.
+ * Space Complexity: O(1) because we have not used any extra space.
+*/
+
+class Solution {
+    // Function to rearrange nodes
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        /*Initialize pointers for odd 
+        and even nodes and keep 
+        track of the first even node*/
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode firstEven = head.next;
+        // Rearranging nodes
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        /* Connect the last odd 
+       node to the first even node*/
+        odd.next = firstEven;
+        return head;
     }
-    System.out.println();
 }
+
+/*
+ * optimized
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even; // Save start of even list
+
+        while (even != null && even.next != null) {
+            odd.next = even.next; // link odd to next odd
+            odd = odd.next; // move odd pointer
+
+            even.next = odd.next; // link even to next even
+            even = even.next; // move even pointer
+        }
+
+        odd.next = evenHead; // join odd list with even list
+        return head;
+    }
+}
+
