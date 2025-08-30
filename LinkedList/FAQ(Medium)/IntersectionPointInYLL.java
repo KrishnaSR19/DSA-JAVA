0f78ab666f39a65d012cdb4497cab03c09e33da7
Krishna <coderks36@gmail.com>
2025-08-30 15:39:21 +0530

    Find the intersection point of Y LL(Better Approach)

--- LinkedList/FAQ(Medium)/IntersectionPointInYLL.java
+++ LinkedList/FAQ(Medium)/IntersectionPointInYLL.java
@@ -4,6 +4,11 @@ The Linked List will not contain any cycles. The linked lists must retain their 
  */
 /*
  * Brute Force(Using hashing)
+ * Time Complexity: O(N + M), where N and M are the lengths of the first and second linked list respectively.
+Traversing the first list and adding the nodes to the hashset takes O(N) time assuming the hashset takes O(1) time for operations. Iterating through all nodes in the second list takes O(M) time. Therefore, the total time complexity is O(N + M).
+Note: If the hashset takes logarithmic time for operations, the time complexity get a multiple of logN.
+Space Complexity: O(N)
+Using an hashset to store the addresses of all nodes in the first list takes O(N) space.
  */
 class Solution {
     // Function to find the intersection node of two linked lists
@@ -30,5 +35,57 @@ class Solution {
         }
         // No intersection found, return null
         return null;
+    }
+}
+
+/*
+ * Better Approach(Saving Space)
+ * Time Complexity: O(N + M), where N and M are the lengths of first and second
+ * linked list respectively.
+ * Calculating the lengths of the two linked list takes O(N) and O(M) time.
+ * Another O(|N-M|) time is needed for aligning the nodes. The final traversal
+ * of the aligned lists takes O(min(N,M)) time in the worst case. Thus, the
+ * overall time complexity is O(N + 2M) or O(N + M).
+ * Space Complexity: O(1), because only a couple of pointers are used.
+ */
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        int n1 = 0, n2 = 0;
+
+        // Get the length of first linked list
+        while (temp1 != null) {
+            n1++;
+            temp1 = temp1.next;
+        }
+
+        // Get the length of second linked list
+        while (temp2 != null) {
+            n2++;
+            temp2 = temp2.next;
+        }
+
+        // Traverse the longer list and bring the pointers to same level
+        if (n1 < n2)
+            return collisionPoint(headA, headB, n2 - n1);
+
+        return collisionPoint(headB, headA, n1 - n2);
+    }
+
+    private ListNode collisionPoint(ListNode smallerListHead, ListNode longerListHead, int len) {
+        ListNode temp1 = smallerListHead;
+        ListNode temp2 = longerListHead;
+
+        // Adjust the pointers to same level
+        for (int i = 0; i < len; i++)
+            temp2 = temp2.next;
+
+        while (temp1 != temp2) {
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        return temp1;
     }
 }
