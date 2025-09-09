f81ad8e32ed2ab03cfd5fe0cf3e37c04463154e9
Krishna <coderks36@gmail.com>
2025-09-09 19:34:27 +0530

    Flattening of LL(Brute Force)

---
+++ LinkedList/FAQ(Hard)/FLattening.java
@@ -0,0 +1,79 @@
+/*
+ * Given a special linked list containing n head nodes where every node in the linked list contains two pointers:
+‘Next’ points to the next node in the list
+‘Child’ pointer to a linked list where the current node is the head
+Each of these child linked lists is in sorted order and connected by a 'child' pointer.
+Flatten this linked list such that all nodes appear in a single sorted layer connected by the 'child' pointer and return the head of the modified list.
+ */
+
+/*Brute Force
+ * Time Complexity:
+We traverse all nodes → O(N×M).
+Sorting the collected elements → O(N×M log(N×M)).
+Reconstructing the linked list → O(N×M).
+Total = O(N×M log(N×M)), since sorting dominates.
+Space Complexity:
+Extra array to store all elements → O(N×M).
+Rebuilding the list also uses → O(N×M).
+Total = O(N×M).
+ */
+
+class Solution {
+    // Function to convert a vector to a linked list
+    private ListNode convertArrToLinkedList(List<Integer> arr) {
+        /*
+         * Create a dummy node to serve as
+         * the head of the linked list
+         */
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+
+        /*
+         * Iterate through the vector and
+         * create nodes with vector elements
+         */
+        for (int i = 0; i < arr.size(); i++) {
+            // Create a new node with the vector element
+            temp.child = new ListNode(arr.get(i));
+
+            // Update the temporary pointer
+            temp = temp.child;
+        }
+
+        /*
+         * Return the linked list starting
+         * from the next of the dummy node
+         */
+        return dummyNode.child;
+    }
+
+    // Function to flatten a linked list with child pointers
+    public ListNode flattenLinkedList(ListNode head) {
+        List<Integer> arr = new ArrayList<>();
+
+        // Traverse through the linked list
+        while (head != null) {
+            /*
+             * Traverse through the child
+             * nodes of each head node
+             */
+            ListNode t2 = head;
+
+            while (t2 != null) {
+                // Store each node's data in the array
+                arr.add(t2.val);
+
+                // Move to the next child node
+                t2 = t2.child;
+            }
+            // Move to the next head node
+            head = head.next;
+        }
+
+        // Sort the array containing node values
+        Collections.sort(arr);
+
+        // Convert the sorted array back to a linked list
+        return convertArrToLinkedList(arr);
+    }
+}
