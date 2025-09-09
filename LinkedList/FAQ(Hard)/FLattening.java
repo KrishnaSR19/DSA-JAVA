0e5f4893ff291e65c50a49307dae64b698a4df5f
Krishna <coderks36@gmail.com>
2025-09-09 22:51:28 +0530

    Flattenig of LL(Optimal)

--- LinkedList/FAQ(Hard)/FLattening.java
+++ LinkedList/FAQ(Hard)/FLattening.java
@@ -77,3 +77,75 @@ class Solution {
         return convertArrToLinkedList(arr);
     }
 }
+
+/*
+ * Optimal
+ * Time Complexity: O(Nx(2M)) ~ O(2NxM) where N is the length of the linked list
+ * along the next pointer and M is the length of the linked list along the child
+ * pointers.
+ * 
+ * The merge operation in each recursive call takes time complexity proportional
+ * to the length of the linked lists being merged as they have to iterate over
+ * the entire lists. Since the vertical depth of the linked lists is assumed to
+ * be M, the time complexity for a single merge operation is proportional to
+ * O(2M).
+ * This operation is performed N number of times (to each and every node along
+ * the next pointer list) hence the resultant time complexity becomes O(Nx2M).
+ * Space Complexity: O(1) as this code uses no external space or additional data
+ * structures to store values. But a recursive stack uses O(N) space to build
+ * the recursive calls for each node along the next pointer list.
+ */
+
+class Solution {
+    private ListNode merge(ListNode list1, ListNode list2) {
+        /*
+         * Create a dummy node as a
+         * placeholder for the result
+         */
+        ListNode dummyNode = new ListNode(-1);
+        ListNode res = dummyNode;
+
+        // Merge the lists based on data values
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                res.child = list1;
+                res = list1;
+                list1 = list1.child;
+            } else {
+                res.child = list2;
+                res = list2;
+                list2 = list2.child;
+            }
+            res.next = null;
+        }
+
+        // Connect the remaining elements if any
+        if (list1 != null) {
+            res.child = list1;
+        } else {
+            res.child = list2;
+        }
+
+        // Break the last node's link to prevent cycles
+        if (dummyNode.child != null) {
+            dummyNode.child.next = null;
+        }
+
+        return dummyNode.child;
+    }
+
+    // Function to flatten a linked list with child pointers
+    public ListNode flattenLinkedList(ListNode head) {
+        // If head is null or there is no next node
+        if (head == null || head.next == null) {
+            return head; // Return head
+        }
+
+        // Recursively flatten the rest of the linked list
+        ListNode mergedHead = flattenLinkedList(head.next);
+
+        // Merge the lists
+        head = merge(head, mergedHead);
+        return head;
+    }
+}
